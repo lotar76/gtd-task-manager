c25839946827a4432d2b0a3ceeef645d82eb8fec
Игорь <lotar76@gmail.com>
2025-11-25 19:43:21 +0300

    Fix: remove localhost from sanctum default domains, fix hardcoded localhost:9090 in frontend build

--- config/sanctum.php
+++ config/sanctum.php
@@ -6,11 +6,9 @@ use Laravel\Sanctum\Sanctum;
 
 return [
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 
         Sanctum::currentApplicationUrlWithPort()
-    ))),
+    )),
 
     'guard' => ['web'],
 
